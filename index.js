e8a8e21831824629da75e711e7a634fbb99140f5
Marco Jardim <marcoeojardim@gmail.com>
2020-09-02 23:17:59 -0300

    download everything

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
-const bent = require('bent');
-const get = bent("http://www.portaltransparencia.gov.br", "buffer", 200, 201, 301, 302);
+const Fs = require('fs')  
+const Path = require('path')  
+const Axios = require('axios')
+const moment = require("moment")
 
+async function downloadFile (date) {  
+  const url = "http://www.portaltransparencia.gov.br/download-de-dados/bolsa-familia-pagamentos/" + date;
+  const path = Path.resolve(__dirname, 'files', date + '_BolsaFamilia_Pagamentos.zip')
+  const writer = Fs.createWriteStream(path)
 
-async function runScraper() {
-    let result = await get("/download-de-dados/bolsa-familia-pagamentos/202001");
-    console.log(result.byteLength);
+  const response = await Axios({
+    url,
+    method: 'GET',
+    responseType: 'stream'
+  })
+
+  response.data.pipe(writer)
+
+  return new Promise((resolve, reject) => {
+    writer.on('finish', resolve)
+    writer.on('error', reject)
+  })
 }
 
-const run = async () => {
-    await runScraper();
+for (year = 2013; year < moment().year(); year++) {
+    for (month = 1; month <= 12; month++) {
+        let date = "" + year + (( '0' + month).substr( -2 ));
+        console.log(date)
+        downloadFile(date)  
+    }
 }
 
-run();
+for (month = 0; month < moment().month(); month++) {
+    let date = "" + moment().year() + (( '0' + (month + 1)).substr( -2 ));
+    console.log(date)
+    downloadFile(date) 
+}
+
+// downloadFile()  
